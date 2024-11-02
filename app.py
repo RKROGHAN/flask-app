68c30558aeb5bb077e8ec40de09310af8b404167
RKROGHAN <roghan911@gmail.com>
2024-11-02 14:21:47 +0530

    app.py

--- app.py
+++ app.py
@@ -55,5 +55,4 @@ def statement():
     return render_template('statement.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
